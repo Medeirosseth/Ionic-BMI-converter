e9c4c643fdb42b1cfc21eaa91c2de7b003658738
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-08-16 20:27:43 -0700

    add results display

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const resetButton = document.getElementById("reset-button");
 const heightInput = document.getElementById("height-input");
 const weightInput = document.getElementById("weight-input");
 
+const result = document.getElementById("result");
+
 const reset = () => {
   heightInput.value = "";
   weightInput.value = "";
@@ -14,7 +16,19 @@ const calculateBMI = () => {
 
   const bmi = enteredHeight / (enteredHeight * enteredWeight);
 
-  console.log(bmi);
+  if (isNaN(bmi)) {
+    alert("Not a number, please check inputs");
+    return;
+  }
+
+  const resultElement = document.createElement("ion-card");
+  resultElement.innerHTML = `
+  <ion-card-content>
+    <h2>${bmi}</h2>
+  </ion-card-content>
+  `;
+  result.innerHTML = "";
+  result.appendChild(resultElement);
 };
 
 convertButton.addEventListener("click", calculateBMI);
